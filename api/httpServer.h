68bc8205d29b9fa2716bbb320cc6ff394579f0f5
Jakub Żmuda <goodguykuba@gmail.com>
2019-06-16 22:53:38 +0200

    hello controller example

--- api/httpServer.h
+++ api/httpServer.h
@@ -1,36 +1,20 @@
-/**
- * test.c
- * Small Hello World! example
- * to compile with gcc, run the following command
- * gcc -o test test.c -lulfius
- */
-#include <stdio.h>
 #include <ulfius.h>
+#include <stdio.h>
+#include "./controllers/helloController.h"
 
 #define PORT 8080
 
-/**
- * Callback function for the web application on /helloworld url call
- */
-int callback_hello_world (const struct _u_request * request, struct _u_response * response, void * user_data) {
-    ulfius_set_string_body_response(response, 200, "Hello World!");
-    return U_CALLBACK_CONTINUE;
-}
-
-/**
- * main function
- */
-int main(void) {
+void runHttpServer() {
     struct _u_instance instance;
 
     // Initialize instance with the port number
     if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
         fprintf(stderr, "Error ulfius_init_instance, abort\n");
-        return(1);
+        exit(1);
     }
 
     // Endpoint list declaration
-    ulfius_add_endpoint_by_val(&instance, "GET", "/helloworld", NULL, 0, &callback_hello_world, NULL);
+    ulfius_add_endpoint_by_val(&instance, "GET", "/hello", NULL, 0, &helloController, NULL);
 
     // Start the framework
     if (ulfius_start_framework(&instance) == U_OK) {
@@ -46,5 +30,4 @@ int main(void) {
     ulfius_stop_framework(&instance);
     ulfius_clean_instance(&instance);
 
-    return 0;
 }
