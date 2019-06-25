49a65b42a788374071ca78fd1104ecca6a2a02be
Jakub Żmuda <goodguykuba@gmail.com>
2019-06-25 23:35:06 +0200

    base 64 support

--- api/httpServer.h
+++ api/httpServer.h
@@ -1,6 +1,7 @@
 #include <ulfius.h>
 #include <stdio.h>
 #include "./controllers/helloController.h"
+#include "./controllers/loginController.h"
 
 #define PORT 8080
 
@@ -15,19 +16,23 @@ void runHttpServer() {
 
     // Endpoint list declaration
     ulfius_add_endpoint_by_val(&instance, "GET", "/hello", NULL, 0, &helloController, NULL);
+    ulfius_add_endpoint_by_val(&instance, "GET", "/login", NULL, 0, &loginController, NULL);
 
     // Start the framework
     if (ulfius_start_framework(&instance) == U_OK) {
         printf("Start framework on port %d\n", instance.port);
-
-        // Wait for the user to press <enter> on the console to quit the application
-        getchar();
     } else {
         fprintf(stderr, "Error starting framework\n");
     }
-    printf("End framework\n");
 
-    ulfius_stop_framework(&instance);
-    ulfius_clean_instance(&instance);
+    // instant shutdown prevention
+    while(1) {
+        getchar();
+    }
+
+//    printf("End framework\n");
+//
+//    ulfius_stop_framework(&instance);
+//    ulfius_clean_instance(&instance);
 
 }
