ae6ea41df375774b3e41bdd46d9f034ad119535d
Jakub Żmuda <goodguykuba@gmail.com>
2019-07-03 11:39:44 +0200

    compact parsing

--- api/controllers/loginController.h
+++ api/controllers/loginController.h
@@ -6,21 +6,12 @@
 
 void loginController(char *requestBody, char responseBody[], char responseStatus[]) {
 
-    json_t *root;
-    json_t *login;
-    json_t *password;
     json_error_t error;
-
-    root = json_loads(requestBody, 0, &error);
-    login = json_object_get(root, "login");
-    password = json_object_get(root, "password");
+    json_t *root = json_loads(requestBody, 0, &error);
+    json_t *login = json_object_get(root, "login");
+    json_t *password = json_object_get(root, "password");
 
     printf("received login %s, password %s\n", json_string_value(login), json_string_value(password));
-
-    if (!root) {
-        printf("error extracting JSON from request body\n");
-        return;
-    }
 
     char *response = "{\"token\": \"eyJpZCI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJyb2xlIjoiQWRtaW4ifQ\"}";
     strncpy(responseBody, response, strlen(response));
