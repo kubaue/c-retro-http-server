451fa2a66aa8293e4204cffaf8af699941a510d3
Jakub Żmuda <goodguykuba@gmail.com>
2019-07-03 11:20:36 +0200

    response codes

--- api/controllers/loginController.h
+++ api/controllers/loginController.h
@@ -1,11 +1,13 @@
 #include <string.h>
 #include "../base64.h"
 #include "../idGenerator.h"
+#include "../http/httpResposeStatus.h"
 
 void loginController(char *requestBody, char responseBody[], char responseStatus[]) {
     char *response = "{\"token\": \"eyJpZCI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJyb2xlIjoiQWRtaW4ifQ\"}";
     strncpy(responseBody, response, strlen(response));
 
-    char *status = "200 OK";
+    char status[statusBufferLength];
+    strcpy(status,responseStatusOK);
     strncpy(responseStatus, status, strlen(status));
 }
---
+++ api/http/httpResposeStatus.h
@@ -0,0 +1,11 @@
+const int statusBufferLength = 16;
+
+const char *responseStatusOK = "200 OK";
+
+const char *responseStatusBadRequest = "400 Bad Request";
+const char *responseStatusUnauthorized = "401 Unauthorized";
+const char *responseStatusForbidden = "403 Forbidden";
+const char *responseStatusNotFound = "404 Not Found";
+
+const char *responseStatusInternalServerError = "500 Internal Server Error";
+const char *responseStatusInternalServiceUnavailable = "503 Service Unavailable";
