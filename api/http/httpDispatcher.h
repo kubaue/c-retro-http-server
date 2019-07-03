74aa939eee84e9ae70ec5e6ab96c669fa4bf6652
Jakub Żmuda <goodguykuba@gmail.com>
2019-07-03 09:53:18 +0200

    parsing http body

--- api/http/httpDispatcher.h
+++ api/http/httpDispatcher.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define BUFFER_LENGTH 16
+#define HTTP_BODY_LENGTH 1024
 
 void extractPath(char dest[], char *httpRequest, int httpMethodLength);
 
@@ -8,17 +9,16 @@ void extractMethod(char dest[], char *httpRequest);
 
 int lengthBeforeNextSpace(const char *inputString);
 
+void extractBody(char dest[], char *httpRequest);
+
 void dispatchHttpRequest(char *httpRequest) {
     char method[BUFFER_LENGTH];
     char path[BUFFER_LENGTH];
+    char body[HTTP_BODY_LENGTH];
 
     extractMethod(method, httpRequest);
     extractPath(path, httpRequest, strlen(method));
-
-    printf("method: |%s|\n", method);
-    printf("path: |%s|\n", path);
-
-    printf("\n%s\n", httpRequest);
+    extractBody(body, httpRequest);
 }
 
 void extractMethod(char dest[], char *httpRequest) {
@@ -31,6 +31,12 @@ void extractPath(char dest[], char *httpRequest, int httpMethodLength) {
 
     memset(dest, '\0', BUFFER_LENGTH);
     strncpy(dest, strippedFromMethodRequest, lengthBeforeNextSpace(strippedFromMethodRequest));
+}
+
+void extractBody(char dest[], char *httpRequest) {
+    memset(dest, '\0', HTTP_BODY_LENGTH);
+    char *stripped = strchr(httpRequest, '{');
+    strncpy(dest, stripped, HTTP_BODY_LENGTH);
 }
 
 int lengthBeforeNextSpace(const char *inputString) {
