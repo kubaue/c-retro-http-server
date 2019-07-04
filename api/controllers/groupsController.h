5bda33edd97b11b82c6bd0585fb912a5949d1121
Jakub Żmuda <goodguykuba@gmail.com>
2019-07-04 19:10:32 +0200

    extracted unauthorized status logic

--- api/controllers/groupsController.h
+++ api/controllers/groupsController.h
@@ -1,6 +1,8 @@
 #include <jansson.h>
 #include "../repositories/groupsRepository.h"
 
+void unauthorizedForGroupManipulations(char *responseStatus);
+
 void createGroupController(char *requestBody, char userRole[], char responseBody[], char responseStatus[]) {
 
     if (strcmp(userRole, "admin") == 0) {
@@ -17,10 +19,34 @@ void createGroupController(char *requestBody, char userRole[], char responseBody
         strncpy(responseStatus, status, strlen(status));
     }
     else {
-        char status[statusBufferLength];
-        memset(status, '\0', statusBufferLength);
-        strcpy(status, responseStatusUnauthorized);
-        memset(responseStatus, '\0', httpBufferLength);
-        strncpy(responseStatus, status, strlen(status));
+        unauthorizedForGroupManipulations(responseStatus);
     }
+}
+
+//void editGroupController(char *requestBody, char userRole[], char responseBody[], char responseStatus[]) {
+//
+//    if (strcmp(userRole, "admin") == 0) {
+//        json_error_t error;
+//        json_t *requestJson = json_loads(requestBody, 0, &error);
+//        json_t *students = json_object_get(requestJson, "students");
+//
+//        const char *group = json_string_value(groupName);
+//
+//        createGroup(group);
+//
+//        char status[statusBufferLength];
+//        strcpy(status, responseStatusOK);
+//        strncpy(responseStatus, status, strlen(status));
+//    }
+//    else {
+//        unauthorizedForGroupManipulations(responseStatus);
+//    }
+//}
+
+void unauthorizedForGroupManipulations(char *responseStatus) {
+    char status[statusBufferLength];
+    memset(status, '\0', statusBufferLength);
+    strcpy(status, responseStatusUnauthorized);
+    memset(responseStatus, '\0', httpBufferLength);
+    strncpy(responseStatus, status, strlen(status));
 }
