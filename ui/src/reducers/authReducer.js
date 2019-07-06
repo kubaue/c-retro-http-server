8941c904a49e6e5a2f3653197f5904140c2844fb
Jakub Żmuda <goodguykuba@gmail.com>
2019-07-06 17:06:35 +0200

    removed stubbed token

--- ui/src/reducers/authReducer.js
+++ ui/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { ActionType } from "../actions/actions";
 
 const initialState = {
-  token: 'eyJpZCI6ICIyMDAwIiwgIm5hbWUiOiAiRnJlZGVyaWNrIEJyb3duIiwgInJvbGUiOiAiZXhhbWluZXIifQ==', // TODO revert
+  token: '',
   attemptFailed: false
 };
 
