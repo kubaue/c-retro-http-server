b1de82cd7e002729e44b45edd881b3235c0a2418
Jakub Żmuda <goodguykuba@gmail.com>
2019-07-06 12:11:12 +0200

    editing question text

--- ui/src/components/pages/CreateExamPage.js
+++ ui/src/components/pages/CreateExamPage.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import styles from './CreateExamPage.module.css';
 import { userData } from '../../selectors/authSelectors';
 import { createExam, fetchGroups } from '../../actions/actions';
+import _ from 'lodash';
 
 class CreateExamPage extends React.Component {
 
@@ -14,7 +15,8 @@ class CreateExamPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      groupId: ''
+      groupId: '',
+      questions: []
     }
   }
 
@@ -26,15 +28,16 @@ class CreateExamPage extends React.Component {
             <div className={styles.group}>
               <label className={styles.label}>Group</label>
               <select onChange={(event) => this.setState({ groupId: event.target.value })} value={this.state.groupId}>
-                <option value={''}/>
+                <option value={''} />
                 {this.props.groups.map(group => <option key={group.id} value={group.id}>{group.groupName}</option>)}
               </select>
             </div>
-            <div className={styles.addQuestionButton}>
-              <button onClick={() => this.addQuestion()}>Add question</button>
+            <div className={styles.questions}>
+              {this.state.questions.map(question => this.renderQuestion(question))}
             </div>
             <div className={styles.createExamContainer}>
-              <button onClick={() => this.createExam()}>Create</button>
+              <button className={styles.button} onClick={() => this.addQuestion()}>Add question</button>
+              <button className={styles.button} onClick={() => this.createExam()}>Create</button>
             </div>
           </div>
         </PageWithRouting>
@@ -48,7 +51,37 @@ class CreateExamPage extends React.Component {
   }
 
   addQuestion() {
+    const localId = _.uniqueId();
+    this.setState(state => ({
+      questions: [...state.questions, {
+        text: '',
+        correctAnswerIndex: '',
+        answers: [],
+        localId: localId
+      }]
+    }))
+  }
 
+  renderQuestion(question) {
+    return (
+      <div key={question.localId} className={styles.question}>
+        <div className={styles.questionRow}>
+          <label>Question</label>
+          <input value={question.text} onChange={(event) => {
+            const newValue = event.target.value;
+            return this.setState(state => ({
+              questions: state.questions.map(q => {
+                if(q.localId === question.localId) {
+                  return ({ ...q, text: newValue });
+                } else {
+                  return q;
+                }
+              })
+            }));
+          }}/>
+        </div>
+      </div>
+    )
   }
 }
 
